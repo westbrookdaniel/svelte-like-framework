0a56c002dedc8255c64e113ff1120a7bd8cb016a
westy12dan <westy12dan@gmail.com>
2023-11-04 15:28:07 +0800

    comp?

--- index.ts
+++ index.ts
@@ -2,34 +2,42 @@ import { readFileSync } from "fs";
 import { parseFragment, serialize } from "parse5";
 import { parse } from "acorn";
 import { generate } from "astring";
-import { createHash, randomBytes } from "crypto";
-// Might need this for reactivity
+import { randomBytes } from "crypto";
+// Might need this for reactivity later?
 // import { walk } from "estree-walker";
 
-const file = readFileSync("./example.hits", "utf8");
+function compile(file: string) {
+  const doc = parseFragment(file);
 
-const doc = parseFragment(file);
+  const scripts: string[] = [];
+  const eventHandlers: string[] = [];
 
-const scripts: string[] = [];
-const eventHandlers: string[] = [];
+  // Parse script tag
+  let ast: any;
+  const i: any = doc.childNodes.findIndex((node) => node.nodeName === "script");
+  if (i !== -1) {
+    const node: any = doc.childNodes[i]!;
+    ast = parse(node.childNodes[0].value, {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    });
+    scripts.push(generate(ast));
+    doc.childNodes.splice(i, 1);
+  }
 
-function traverse(parent: any) {
-  for (let i = 0; i < parent.childNodes.length; i++) {
-    const node = parent.childNodes[i];
-    if (node.nodeName === "script") {
-      const script = node as any; // TODO: Validate
+  function traverse(parent: any) {
+    for (let i = 0; i < parent.childNodes.length; i++) {
+      const node = parent.childNodes[i];
 
-      const ast = parse(script.childNodes[0].value, {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      });
+      // Handle replacing {var} with ${var}
+      if (node.nodeName === "#text") {
+        node.value = node.value.replace(
+          /{(.+?)}/g,
+          (_: any, p1: any) => `\${${p1}}`,
+        );
+      }
 
-      scripts.push(generate(ast));
-
-      parent.childNodes.splice(i, 1);
-    } else {
-      if (node.childNodes) traverse(node);
-
+      // Handle @event handlers
       if (node.attrs) {
         const nodeEventHandlers: any[] = [];
 
@@ -66,13 +74,29 @@ function traverse(parent: any) {
           );
         }
       }
+
+      if (node.childNodes) traverse(node);
     }
   }
+
+  traverse(doc);
+
+  let js = (scripts.join("\n") + "\n" + eventHandlers.join("\n")).trim();
+  if (!js.endsWith(";")) js += ";";
+
+  const html = serialize(doc).trim();
+
+  const transpiler = new Bun.Transpiler({
+    loader: "js",
+    minifyWhitespace: true,
+  });
+
+  return transpiler.transformSync(
+    `export default function component({ $render, ...props }) {${js}return \`${html}\`;};`,
+  );
 }
-traverse(doc);
 
-const js = scripts.join("\n") + "\n" + eventHandlers.join("\n");
-const html = serialize(doc).trim();
+const file = readFileSync("./example.hits", "utf8");
+const out = compile(file);
 
-console.log(html);
-console.log(js);
+console.log(out);
